dac8a74d102611b1e1f963857e86a7c320fa6e27
sujan76 <78744638+sujan76@users.noreply.github.com>
2021-05-22 20:33:29 +0545

    Add files via upload

---
+++ sujantechnical.js
@@ -0,0 +1,8 @@
+function getNames() {
+    // get names from the database or API
+    let firstName = 'John',
+        lastName = 'Doe';
+
+    // return as an array
+    return [firstName, lastName];
+}
